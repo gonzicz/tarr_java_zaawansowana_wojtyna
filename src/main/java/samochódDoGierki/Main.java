3ae2e3391d05961199b095816f4fe9611417e278
gonzicz <gonzicz83@gmail.com>
2019-02-26 18:17:12 +0100

    Dodanie komentarzy w klasie Main

--- src/main/java/samochódDoGierki/Main.java
+++ src/main/java/samochódDoGierki/Main.java
@@ -1,17 +1,18 @@
 package samochódDoGierki;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Track tor1 = new Track(100);
-        Car car1 = new Car(tor1,0);
-        Car car2 = new Car(tor1,10 );
-        System.out.println(tor1);
-        for (int i = 1; i <=10 ; i++)
-        {
-            Thread.sleep(500);  // opóźnienie pół sekundy
-            car1.move(1);
-            System.out.println(tor1);
+        Track tor1 = new Track(100);       // utworzenie nowego toru o rozmiarze 100 (100 pozycji),
+                                                // obiekt po którym jeżdzą auta
+        Car car1 = new Car(tor1, 0);    // tworzenie obiektu (auta) na torze na pozycji 0
+        Car car2 = new Car(tor1, 10);   // tworzenie obiektu (auta) na torze na pozycji 10
+        System.out.println(tor1);               // wyświetlenie aktualnego stanu toru
+
+        // pętla przemieszczająca 10 razy car1 o jedno pole
+        for (int i = 1; i <= 10; i++) {
+            Thread.sleep(500);      // opóźnienie pół sekundy
+            car1.move(1);         // przesunięce auta o jedno pole
+            System.out.println(tor1);     // wyświetlenie aktualnego stanu toru
         }
     }
 }
