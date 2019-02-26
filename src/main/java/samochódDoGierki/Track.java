495c6ae7ac38e12b2982872282b2544fb128fe3c
gonzicz <gonzicz83@gmail.com>
2019-02-26 18:51:04 +0100

    Dodanie komentarzy w klasie Track

--- src/main/java/samochódDoGierki/Track.java
+++ src/main/java/samochódDoGierki/Track.java
@@ -1,39 +1,41 @@
 package samochódDoGierki;
 
-
 /**
  * ctr+shift+F12
  */
-public class Track {
-    private Car[] cars;
 
-    public Track(int size) {
-        this.cars = new Car[size];
+// klasa Track służy do stworzenia toru po której przemieszczają się auta
+public class Track {
+    private Car[] cars;     // deklaracja tablicy, któa może zawierać samochody, jeśli na pozycji jest null to na tej
+                            // pozycji nie ma samochodu, a jeśli nie jest nulll to znajduje się tam samochód
+
+    public Track(int size) {            // konstruktor toru, który tworzy tor o podanej długości
+        this.cars = new Car[size];      // alokacja pamięci na tablicę o rozmiarze size, wszystkie elementy tablicy domyślnie są null
     }
 
-    @Override
+    @Override   // implementacja zamiany tor na stringa w celu przedstawiwnia jej graficznie
     public String toString() {
         String viewCar = "";
         for (int i = 0; i < cars.length; i++) {
             if (cars[i] == null) {
                 viewCar += "_";
             } else {
-                viewCar +="C";
+                viewCar += "C";
             }
         }
         return viewCar;
     }
 
+    // metoda ustawiająca nam samochód na danej pozycji
     public void setCarAt(int position, Car car) {
         cars[position] = car;
     }
 
-
+    // metoda sprawdzająca czy na nowej pozycji jest samochód
     public boolean isCarAt(int newPosition) {
 //        if(cars[newPosition] == null)
 //        {
-//            return false;                     // <-- inne rozwiązanie
-//        }
+//            return false;                     // <-- inne rozwiązanie tego samego
 //        else
 //        {
 //            return true;
