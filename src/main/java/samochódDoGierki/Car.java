07bd2d20f2599fc46566514f022934883fa27fd6
gonzicz <gonzicz83@gmail.com>
2019-02-26 18:41:05 +0100

    Dodanie komentarzy w klasie Car

--- src/main/java/samochódDoGierki/Car.java
+++ src/main/java/samochódDoGierki/Car.java
@@ -1,32 +1,40 @@
 package samochódDoGierki;
 
+// klasa modelująća samochód
 public class Car {
-    private String color;
+    private String color;       // deklaracja pól opisujących atrybuty samochodu
     private Wheel[] kola;
     private Engine silnik;
     private int power;
     private Track track;
     private int position;
 
-    public Car(Track track, int position) {
-        this.track = track;
+    public Car(Track track, int position) {     // konstrukktor klasy Car, który przyjmuje argumenty tor i pozycja
+        this.track = track;                     // this - referncja do TEGO (aktualnego) obiektu
         this.position = position;
-        track.setCarAt(position, this);
+        track.setCarAt(position, this);     // samochód który strzorzymy dodajemy do toru na danej pozycji
     }
 
+
+    // metoda przesuwająca nam auto o podany dystans
     public void move(int distance) {
+        // obliczenie na jakiej pozycji znalazłby się samochód po przejechaniu danego dystansu
         int newPosition = position + distance;
+        // detekcja kolizji, srawdza czy na tej pozycji na którą by się przemieszczał nie znajnduje się inne auto
         if (track.isCarAt(newPosition)) {
             System.out.println("\nBOOM!!!!!");
-            track.setCarAt(position, null);
-            track.setCarAt(newPosition, null);
+            // jeśłi jest kolizja , rozwala samochody czyli usuwa je z toru
+            track.setCarAt(position, null);     // ten
+            track.setCarAt(newPosition, null);  // ten który już jest na nowej pozycji
         } else {
+
             System.out.printf("Samochód został przestawiony z pozycji %d na pozycję %d\n",
                     position, newPosition);
-            track.setCarAt(position, null);
-            track.setCarAt(newPosition, this);
-            position = newPosition;
+            // jeśli nie ma kolizji, to przemieszczamy samochód
+            track.setCarAt(position, null);         // na starej pozycji stawiamy null - usuwamy auto
+            track.setCarAt(newPosition, this);      // umieszczany samochów na nowej pozycji na torze
+            position = newPosition;                     // aktualizujemy obecną pozycję samochodu, ( będzeie potzrebny do
+                                                        // obliczeń kolejnego ruchu)
         }
-
     }
 }
